905ac1b83250023cd77f8f1340a000ec29067715
artdevesa7 <63007186+artdevesa7@users.noreply.github.com>
2020-04-06 13:39:27 -0400

    Add files via upload

---
+++ character_count.js
@@ -0,0 +1,5 @@
+var tweet = prompt("Compose your tweet:");
+var tweetCount = tweet.length;
+alert("You have written "+ tweetCount + " characters, you have " + (140-tweetCount) + " characters remaining");
+
+// you have written 5 characters, you have 135 characters remaining
